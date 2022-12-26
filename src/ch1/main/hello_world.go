2dea95a86b821db3e34145e7a89dc4482bd597aa
Jiean Yang <jiean.yang.freelancer@gmail.com>
2022-12-26 15:24:46 +0100

    Add comments

--- src/ch1/main/hello_world.go
+++ src/ch1/main/hello_world.go
@@ -1,7 +1,13 @@
-package main
+package main // 包，表明代码所在的模块（包）
 
-import "fmt"
+import "fmt" // 引用代码以来
 
+// 功能实现
 func main() {
 	fmt.Println("Hello World")
 }
+
+// **应用程序入口**
+// 1. 必须是 main 包： package main
+// 2. 必须是 main 方法：func main()
+// 3. 文件名不一定是 main.go
