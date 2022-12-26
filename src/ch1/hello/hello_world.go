5fcc027bdec52678e079e633f7609aad7703fb1c
Jiean Yang <jiean.yang.freelancer@gmail.com>
2022-12-26 15:34:30 +0100

    Test args from command line args

--- src/ch1/hello/hello_world.go
+++ src/ch1/hello/hello_world.go
@@ -7,8 +7,12 @@ import (
 
 // 功能实现
 func main() {
-	fmt.Println("Hello World")
-	os.Exit(0)
+	// fmt.Println(os.Args)
+	if len(os.Args)>1  {
+		fmt.Println("Hello World", os.Args[1]) // 打印从命令行进入的参数
+	}
+	// fmt.Println("Hello World")
+	// os.Exit(0)
 }
 
 // **应用程序入口**
