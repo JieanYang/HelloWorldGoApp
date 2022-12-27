5c29035d3acca399f1b0772ee7094e2f36e5bdb0
Jiean Yang <jiean.yang.freelancer@gmail.com>
2022-12-27 23:34:41 +0100

    Handle error in error_test.go

--- src/ch14/error/error_test.go
+++ src/ch14/error/error_test.go
@@ -1,16 +1,32 @@
 package error_test
 
-import "testing"
+import (
+	"errors"
+	"testing"
+)
 
-func GetFibonacci(n int) []int {
+func GetFibonacci(n int) ([]int, error) {
+	if n < 2 || n > 100 {
+		return nil, errors.New("n should be in [2, 100]")
+	}
+
 	fibList := []int{1, 1}
 
 	for i := 2; /*短变量声明 := */ i < n; i++ {
 		fibList = append(fibList, fibList[i-2]+fibList[i-1])
 	}
-	return fibList
+	return fibList, nil
 }
 
 func TestGetFibnacci(t *testing.T) {
-	t.Log(GetFibonacci(10))
+	if value, err := GetFibonacci(10); err != nil {
+		t.Error(err)
+	} else {
+		t.Log(value)
+	}
+	if value, err := GetFibonacci(-10); err != nil {
+		t.Error(err)
+	} else {
+		t.Log(value)
+	}
 }
