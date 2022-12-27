e9cb6b5b2b122362be779125cacd97fc91c05c5f
Jiean Yang <jiean.yang.freelancer@gmail.com>
2022-12-27 23:31:55 +0100

    Create first version of error_test.go

---
+++ src/ch14/error/error_test.go
@@ -0,0 +1,16 @@
+package error_test
+
+import "testing"
+
+func GetFibonacci(n int) []int {
+	fibList := []int{1, 1}
+
+	for i := 2; /*短变量声明 := */ i < n; i++ {
+		fibList = append(fibList, fibList[i-2]+fibList[i-1])
+	}
+	return fibList
+}
+
+func TestGetFibnacci(t *testing.T) {
+	t.Log(GetFibonacci(10))
+}
