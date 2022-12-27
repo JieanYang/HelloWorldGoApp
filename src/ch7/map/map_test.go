407c9cd66b1edf15f2f1340eb205768a92012147
Jiean Yang <jiean.yang.freelancer@gmail.com>
2022-12-27 10:14:50 +0100

    Add codes in map_test.go

--- src/ch7/map/map_test.go
+++ src/ch7/map/map_test.go
@@ -20,3 +20,23 @@ func TestInitMap(t *testing.T) {
 	t.Logf("len m3=%d", len(m3))
 	// t.Logf("cap m3=%d", cap(m3)) // Not support
 }
+
+func TestAccessNotExistingKey(t *testing.T) { // How to know a key exists or not ?
+	m1 := map[int]int{}
+	t.Log(m1[1])
+	m1[2] = 0
+	t.Log(m1[2])
+	// m1[3] = 0
+	if value, isExist := m1[3]; isExist {
+		t.Logf("Key 3's value is %d", value)
+	} else {
+		t.Log("key 3 is not existing.")
+	}
+}
+
+func TestTravelMap(t *testing.T) {
+	m1 := map[int]int{1: 1, 2: 4, 3: 9}
+	for key, value := range m1 {
+		t.Logf("key is %d, value is %d", key, value)
+	}
+}
