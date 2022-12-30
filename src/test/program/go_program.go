8b3fefc615d1eb205817de87f5a9d27e84d78c17
Jiean Yang <jiean.yang.freelancer@gmail.com>
2022-12-30 11:23:43 +0100

    Update port

--- src/test/program/go_program.go
+++ src/test/program/go_program.go
@@ -8,7 +8,7 @@ import (
 func main() {
 	http.HandleFunc("/", rootHandler)
 
-	err := http.ListenAndServe(":9000", nil)
+	err := http.ListenAndServe(":9001", nil)
 	if err != nil {
 		panic(err)
 	}
