aa3eabfc8766d2a689bf50d8151fb2262c8ae3ff
Jiean Yang <jiean.yang.freelancer@gmail.com>
2023-01-11 17:17:17 +0100

    Use context package to cancel tasks

---
+++ src/ch20_2/cancel_by_context_test/cancel_by_context_test.go
@@ -0,0 +1,34 @@
+package cancel_by_context_test
+
+import (
+	"context"
+	"fmt"
+	"testing"
+	"time"
+)
+
+func isCancelled(ctx context.Context) bool {
+	select {
+	case <-ctx.Done():
+		return true
+	default:
+		return false
+	}
+}
+
+func TestCancel1(t *testing.T) {
+	ctx, concel := context.WithCancel(context.Background())
+	for i := 0; i < 5; i++ {
+		go func(i int, ctx context.Context) {
+			for {
+				if isCancelled(ctx) {
+					break
+				}
+				time.Sleep(time.Millisecond * 5)
+			}
+			fmt.Println(i, "Cancelled")
+		}(i, ctx)
+	}
+	concel()
+	time.Sleep(time.Second * 1)
+}
