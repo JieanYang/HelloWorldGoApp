b444f1441e74593d96381dd3a219a1eac7cb64da
Jiean Yang <jiean.yang8534@gmail.com>
2023-01-11 13:59:38 +0100

    Create TestCloseChannel_with_close

--- src/ch20/channel_close/channel_close_test.go
+++ src/ch20/channel_close/channel_close_test.go
@@ -1,38 +1,10 @@
-package csp_test
+package channel_close_test
 
 import (
 	"fmt"
 	"sync"
 	"testing"
-	"time"
 )
-
-func service() string {
-	time.Sleep(time.Millisecond * 50)
-	// time.Sleep(time.Millisecond * 500)
-	return "service - Done"
-}
-
-func AsyncServiceBufferModel() chan string {
-	retCh := make(chan string, 1)
-	go func() {
-		ret := service()
-		fmt.Println("AsyncService - returned result.")
-		retCh <- ret
-		fmt.Println("AsyncService - service exited.")
-	}()
-
-	return retCh
-}
-
-func TestSelect(t *testing.T) {
-	select {
-	case ret := <-AsyncServiceBufferModel():
-		t.Log(ret)
-	case <-time.After(time.Millisecond * 100):
-		t.Error("Time out")
-	}
-}
 
 func dataProducer(ch chan int, wg *sync.WaitGroup) {
 	go func() {
@@ -60,5 +32,39 @@ func TestCloseChannel(t *testing.T) {
 	dataProducer(ch, &wg)
 	wg.Add(1)
 	dataReceiver(ch, &wg)
+	wg.Wait()
+}
+
+func dataProducer_with_close(ch chan int, wg *sync.WaitGroup) {
+	go func() {
+		for i := 0; i < 10; i++ {
+			ch <- i
+		}
+		close(ch)
+		// ch <- 11 // 往关闭的 channel 发消息会收到一个 panic
+		wg.Done()
+	}()
+}
+
+func dataReceiver_estimate_close(ch chan int, wg *sync.WaitGroup) {
+	go func() {
+		for i := 0; i < 10; i++ {
+			if data, ok := <-ch; ok {
+				fmt.Println(data)
+			} else {
+				break
+			}
+		}
+		wg.Done()
+	}()
+}
+
+func TestCloseChannel_with_close(t *testing.T) {
+	var wg sync.WaitGroup
+	ch := make(chan int)
+	wg.Add(1)
+	dataProducer_with_close(ch, &wg)
+	wg.Add(1)
+	dataReceiver_estimate_close(ch, &wg)
 	wg.Wait()
 }
