6a47b2045a9bf6ab8f5d375e0de7f675834e7401
Jiean Yang <jiean.yang.freelancer@gmail.com>
2023-01-11 19:00:18 +0100

    Add more comments

--- src/ch33/obj_cache/sync_pool_test.go
+++ src/ch33/obj_cache/sync_pool_test.go
@@ -18,7 +18,9 @@ func TestSyncPool(t *testing.T) {
 	v := pool.Get().(int) // 断言类型
 	fmt.Println(v)
 	pool.Put(3)
-	// runtime.GC() // GC 会清除 sync.pool 中缓存的对象
+	// runtime.GC() // GC 会清除 sync.pool 中缓存的对象, 这里的描述与测试不一样，有可能是因为 Go 后期版本对特性做了修改
 	v1, _ := pool.Get().(int)
 	fmt.Println(v1)
+	v2, _ := pool.Get().(int) // Get 拿出后，池里就没数据了，所以需要重新产生
+	fmt.Println(v2)
 }
