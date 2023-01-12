c43792430211e9fb22f5b88e398f529ffe3e0d7d
Jiean Yang <jiean.yang.freelancer@gmail.com>
2023-01-12 16:53:43 +0100

    Create TestFillNameAndAge by reflect knowledge

--- src/ch38/flexible_reflect_test.go
+++ src/ch38/flexible_reflect_test.go
@@ -1,10 +1,17 @@
 package flexible_reflect_test
 
 import (
+	"errors"
 	"fmt"
 	"reflect"
 	"testing"
 )
+
+type Employee struct {
+	EmployeeID string
+	Name       string `format:"normal"`
+	Age        int
+}
 
 type Customer struct {
 	CookieID string
@@ -33,4 +40,55 @@ func TestDeepEqual(t *testing.T) {
 	c2 := Customer{"1", "Mike", 40}
 	fmt.Println("c1 == c2?", c1 == c2)
 	fmt.Println("c1 == c2 DeepEqual ?", reflect.DeepEqual(c1, c2))
+}
+
+func fillBySettings(st interface{}, settings map[string]interface{}) error {
+	// func (v Value) Elem Value
+	// Elem returns the value that the interface v contains or that the pointer
+	//  it panics if v's kind is not Interface or Ptr.
+	// It returns the zero if Value is nil.
+
+	if reflect.TypeOf(st).Kind() != reflect.Ptr {
+		// Elem() 获得指针指向的值
+		if reflect.TypeOf(st).Elem().Kind() != reflect.Struct {
+			return errors.New("The first param should be a pointer to the struct type")
+		}
+	}
+
+	if settings == nil {
+		return errors.New("settings is nil.")
+	}
+
+	var (
+		field reflect.StructField
+		ok    bool
+	)
+
+	for k, v := range settings {
+		if field, ok = (reflect.ValueOf(st)).Elem().Type().FieldByName(k); !ok {
+			continue
+		}
+
+		if field.Type == reflect.TypeOf(v) {
+			vstr := reflect.ValueOf(st)
+			vstr = vstr.Elem()
+			vstr.FieldByName(k).Set(reflect.ValueOf(v))
+		}
+	}
+
+	return nil
+}
+
+func TestFillNameAndAge(t *testing.T) {
+	settings := map[string]interface{}{"Name": "Mike", "Age": 40}
+	e := Employee{}
+	if err := fillBySettings(&e, settings); err != nil {
+		t.Fatal(err)
+	}
+	t.Log(e)
+	c := new(Customer)
+	if err := fillBySettings(c, settings); err != nil {
+		t.Fatal(err)
+	}
+	t.Log(*c)
 }
