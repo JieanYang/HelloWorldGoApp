51f12699ae2daa279b091bd0a63a9be5e65c3feb
Jiean Yang <jiean.yang.freelancer@gmail.com>
2023-01-12 16:24:00 +0100

    Update reflect_test.go

--- src/ch37/reflect/reflect_test.go
+++ src/ch37/reflect/reflect_test.go
@@ -28,3 +28,33 @@ func TestBasicType(t *testing.T) {
 	var f float64 = 12
 	CheckType(f)
 }
+
+type Employee struct {
+	EmployeeID string
+	Name       string `format:"normal"`
+	Age        int
+}
+
+func (e *Employee) UpdateAge(newVal int) {
+	e.Age = newVal
+}
+
+type Customer struct {
+	CookieID string
+	Name     string
+	Age      int
+}
+
+func TestInvokeByName(t *testing.T) {
+	e := &Employee{"1", "Mike", 30}
+	// 按名字获取成员
+	t.Logf("Name: value(%[1]v), Type(%[1]T)", reflect.ValueOf(*e).FieldByName("Name"))
+	if nameField, ok := reflect.TypeOf(*e).FieldByName("Name"); !ok {
+		t.Error("Failed to get 'Name' field.")
+	} else {
+		t.Log("Tag:format is:", nameField.Tag.Get("format"))
+	}
+	reflect.ValueOf(e).MethodByName("UpdateAge").
+		Call([]reflect.Value{reflect.ValueOf(1)})
+	t.Log("Updated Age:", e)
+}
