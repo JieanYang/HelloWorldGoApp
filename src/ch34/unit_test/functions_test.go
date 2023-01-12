cd5d586e7d09a74980892c1e0e799a42a9edd093
Jiean Yang <jiean.yang.freelancer@gmail.com>
2023-01-12 15:22:18 +0100

    Use 'github.com/stretchr/testify/assert' package

--- src/ch34/unit_test/functions_test.go
+++ src/ch34/unit_test/functions_test.go
@@ -3,6 +3,8 @@ package unit_test_test
 import (
 	"fmt"
 	"testing"
+
+	"github.com/stretchr/testify/assert"
 )
 
 func TestSquare(t *testing.T) {
@@ -27,4 +29,17 @@ func TestFailInCode(t *testing.T) {
 	fmt.Println("Start")
 	t.Fatal("Error")
 	fmt.Println("End")
+}
+
+func TestSquareWithAssert(t *testing.T) {
+	inputs := [...]int{1, 2, 3}
+	expected := [...]int{1, 4, 9}
+
+	for i := 0; i < len(inputs); i++ {
+		ret := square(inputs[i])
+		assert.Equal(t, expected[i], ret) // replace codes below
+		// if ret != expected[i] {
+		// 	t.Errorf("input is %d, the expected is %d, the actual is %d", inputs[i], expected[i], ret)
+		// }
+	}
 }
